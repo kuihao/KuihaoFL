63ea2616651583d0ef285922142d250d3f1de219
kuihao <kuihaochang@gmail.com>
2021-11-03 21:40:23 +0800

    generate the dimenation shape picture of resnet18

--- Implement_google_AdaptiveFL/test2.py
+++ Implement_google_AdaptiveFL/test2.py
@@ -3,4 +3,4 @@ from mymodel.resnet18 import ResNet18
 
 model = ResNet18((32,32,3),10)
 #model.summary()
-tf.keras.utils.plot_model(model, "resnet18.png")
+tf.keras.utils.plot_model(model, "resnet18_with_shape.png", show_shapes=True)
