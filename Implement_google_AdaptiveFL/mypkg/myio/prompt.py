cdb5eff5e3cca5d3d599c5a41713056ac8f6d52b
kuihao <kuihaochang@gmail.com>
2021-11-05 13:45:03 +0800

    upgrade to commandline version

--- Implement_google_AdaptiveFL/mypkg/myio/prompt.py
+++ Implement_google_AdaptiveFL/mypkg/myio/prompt.py
@@ -1,7 +1,36 @@
 from datetime import datetime
+import argparse
 
-def KeepPriorTrain(UniqueModelName=''):
-    '''Welcome prompt: keep prior-training?'''
+def ServerArg():
+    '''To get the learning setting info. via commandline args'''
+    parser = argparse.ArgumentParser(description="Flower Server")
+    parser.add_argument("-m", "--mode", 
+                        type=int, choices=range(0, 3), required=True,
+                        help="\"0\" to start a new trainig,\n"
+                             "\"1\" to recovery this model from prior record,\n"\
+                             "\"2\" to load other checkpoints (weights).\n")
+    parser.add_argument("-n", "--name", 
+                        type=str, required=False,
+                        help="Give model a unique name.")
+    parser.add_argument("-pp", "--prior_path", 
+                        type=str, required=False,
+                        help="If model = 1, input the prior model storage file path.")
+    parser.add_argument("-cp", "--checkpoint_path", 
+                        type=str, required=False,
+                        help="If model = 2, input the other checkpoint storage file path.")
+    parser.add_argument("--cpu",
+                    action="store_true",  # 引數儲存為 boolean
+                    help="使否只使用CPU? 此為引數為Flag，後方不能再輸入其他內容")
+    args = parser.parse_args()
+
+    if args.mode == 1 and args.prior_path is None:
+        raise Exception("If model = 1, --prior_path (-pp) is required.")
+    elif args.mode == 2 and args.checkpoint_path is None:
+        raise Exception("If model = 2, --checkpoint_path (-cp) is required.")
+    return args
+
+def ModelNameGenerator(UniqueName=None):
+    '''Welcome prompt and generate the model name with timestemp'''
     print("  ___ _   _               _                      _   _  __       _       _                           _       _     _       ")
     print(" |_ _| |_( )___    __ _  | |__   ___  __ _ _   _| |_(_)/ _|_   _| |   __| | __ _ _   _    ___  _   _| |_ ___(_) __| | ___  ")
     print("  | || __|// __|  / _` | | '_ \ / _ \/ _` | | | | __| | |_| | | | |  / _` |/ _` | | | |  / _ \| | | | __/ __| |/ _` |/ _ \ ")
@@ -9,34 +38,8 @@ def KeepPriorTrain(UniqueModelName=''):
     print(" |___|\__| |___/  \__,_| |_.__/ \___|\__,_|\__,_|\__|_|_|  \__,_|_|  \__,_|\__,_|\__, |  \___/ \__,_|\__|___/_|\__,_|\___| ")
     print("                                                                                 |___/                                     ")
     print("-------------")
-
     now_time = datetime.now() # current date and time
     time_str = now_time.strftime("%m_%d_%Y__%H_%M_%S")
-  
-    input_error = True # default
-    model_version_name = time_str+"_"+UniqueModelName
-    new_cppath = ''
-    while(input_error):
-        train_mode = int(input("Input:\n\"0\" to start a new trainig,\n"
-        "\"1\" to recovery this model from prior record,\n"\
-        "\"2\" to load other checkpoints (weights).\n"))
-        if train_mode == 0:
-            print("let's play a new game!")
-            input_error = False
-        elif train_mode == 1:
-            print("Keep prior-traing going~~~")
-            model_version_name = input("Prior model version name?\n")
-            input_error = False
-        elif train_mode == 2:
-            print("What a great idea! Let's put something new to it.")
-            #model_version_name = input("Prior model version name?\n")
-            new_cppath = input("Where's the new checkpoint path?(forder name)\n")
-            input_error = False
-        else:
-            input_error = True
-
-    return train_mode, model_version_name, new_cppath
-
-#train_mode, model_version_name = KeepPriorTrain()
-#print(train_mode)
-#print(model_version_name)
+    '' if UniqueName is None else UniqueName
+    model_name = time_str+"_"+UniqueName
+    return model_name
