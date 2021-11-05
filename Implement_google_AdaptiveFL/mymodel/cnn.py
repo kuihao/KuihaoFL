11ea2e1a6b298f10998dd703c14cef2f2d9bdf6d
kuihao <kuihaochang@gmail.com>
2021-11-05 10:44:48 +0800

    add my simple cnn

--- Implement_google_AdaptiveFL/mymodel/cnn.py
+++ Implement_google_AdaptiveFL/mymodel/cnn.py
@@ -1,10 +1,11 @@
 from tensorflow.keras import Input, Model, layers
 
-def CNN_Model(input_shape, number_classes):
-  # define Input layer
-  input_tensor = Input(shape=input_shape) # Input: convert normal numpy to Tensor (float32)
+def CNN_Model(input_shape, ClassNum):
+  # Step 1. Create a input node
+  input_tensor = Input(shape=input_shape)
+  '''Input: convert normal numpy to Tensor (float32)'''
 
-  # define layer connection
+  # Steop 2. Add more layers
   x = layers.Conv2D(filters = 32, kernel_size=(3, 3), activation="relu")(input_tensor)
   x = layers.MaxPooling2D(pool_size=(2, 2))(x)
   x = layers.Conv2D(filters = 64, kernel_size=(3, 3), activation="relu")(x)
@@ -14,8 +15,11 @@ def CNN_Model(input_shape, number_classes):
   x = layers.Flatten()(x)
   #x = layers.Dropout(0.5)(x)
   x = layers.Dense(64, activation='relu')(x)
-  outputs = layers.Dense(number_classes)(x) #, activation="softmax"
+  
+  # Step 3. Create a output node
+  outputs = layers.Dense(ClassNum)(x) #, activation="softmax"
 
-  # define model
+  # Step 4. Create a  Model to specifying its 
+  # inputs and outputs in the graph of layers.
   model = Model(inputs=input_tensor, outputs=outputs, name="TF_Tutorials_CNN_Model")
   return model
