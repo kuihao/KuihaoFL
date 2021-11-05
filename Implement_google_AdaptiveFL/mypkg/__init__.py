5135893d00f9aec47f412cd20e242fa111864f75
kuihao <kuihaochang@gmail.com>
2021-11-05 13:45:46 +0800

    modified import ini.

--- Implement_google_AdaptiveFL/mypkg/__init__.py
+++ Implement_google_AdaptiveFL/mypkg/__init__.py
@@ -3,12 +3,13 @@ from .myFedAda.FedAdam import MyFedAdam
 from .myFedAda.FedAdagrad import MyFedAdagrad
 from .gpu.limitGPU import setGPU
 from .myio.PathSetting import secure_mkdir
-from .myio.prompt import KeepPriorTrain
+from .myio.prompt import ServerArg, ModelNameGenerator
 __all__ = [
     "MyFedYogi",
     "MyFedAdam",
     "MyFedAdagrad",
     "setGPU",
     "secure_mkdir",
-    "KeepPriorTrain",
+    "ServerArg",
+    "ModelNameGenerator",
 ]
