5d8857947e894fa645517a9c68f8a0bffbf67cef
kuihao <kuihaochang@gmail.com>
2021-11-18 18:09:37 +0800

    add preprocessed.py

--- Implement_google_AdaptiveFL/mypkg/TF/__init__.py
+++ Implement_google_AdaptiveFL/mypkg/TF/__init__.py
@@ -1,8 +1,12 @@
 from .gpu.limitGPU import setGPU
 from .mymodel.cnn import CNN_Model
 from .mymodel.resnet18 import myResNet
+from .mymodel.loadds import myLoadDS
+from .mymodel.preprocess import GoogleAdaptive_tfds_preprocess
 __all__ = [
     "setGPU",
     "CNN_Model",
     "myResNet",
+    "myLoadDS",
+    "GoogleAdaptive_tfds_preprocess"
 ]
---
+++ Implement_google_AdaptiveFL/mypkg/TF/mymodel/preprocess.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+class GoogleAdaptive_tfds_preprocess():
+    def __init__(self):
+        self.dataset=None
+        self.rng=None
+        self.train=True
+        self.global_seed=2021
+        self.crop_size=24 # 欲裁切的大小
+        self.num_client=500
+        self.batch_zize=20
+        self.shuffle_buffer=100
+        self.prefetch_buffer=20
+
+    def preprocess(self, dataset, rng, train,
+                   global_seed, crop_size, 
+                   num_client, batch_zize,
+                   shuffle_buffer, prefetch_buffer,
+                  ):
+        """
+        輸入
+        一個client的資料集，內部進行shuffle、batching、preprocessing
+        輸出
+        一個標記如何切割、epoch、batching、shuffle、預處理後的 
+        single client Dataset:tf.PrefetchDataset:tuple(batched)"""
+        self.dataset=dataset
+        self.rng=rng
+        self.train=train
+        self.global_seed=global_seed
+        self.crop_size=crop_size # 欲裁切的大小
+        self.num_client=num_client
+        self.batch_zize=batch_zize
+        self.shuffle_buffer=shuffle_buffer
+        self.prefetch_buffer=prefetch_buffer
+        
+        def simlpe_rescale(self, image, label):
+            '''基本圖像正規化 (batch_format_fn)'''
+            # for IMAGE
+            image = tf.cast(image, tf.float32)
+            # 論文要求的 Z-score 正規化 (mean and standard deviation)
+            image_mean = tf.math.reduce_mean(image)
+            image_std = tf.math.reduce_std(image,1)
+            image = ((image-image_mean) / image_std)
+
+            # for LABEL
+            tf.reshape(label, [-1, 1])
+            return image, label
+
+        def custom_augment(self, element):
+            '''自訂資料增強 (batch_format_fn)'''
+            global crop_size
+            (image, label) = (element['image'], element['label'])
+
+            # 套用基本圖像正規化
+            image, label = simlpe_rescale(image, label)
+            
+            # 像素正規化
+            # 只有 Training 才套用
+            if(train):
+                # 欲裁切的大小
+                crop_size = 24
+                
+                # 隨機裁切成 crop_size
+                seed = rng.make_seeds(2)[0]
+                image = tf.image.stateless_random_crop(image, size=[crop_size, crop_size, 3], seed=seed)
+                image = tf.image.stateless_random_flip_left_right(image, seed=seed)
+
+            # Testing時套用
+            else:
+                # Centrally Cropping (此函式的裁切自動裁中心)
+                image = tf.image.resize_with_crop_or_pad(image, crop_size, crop_size)
+            return (image, label) 
+
+        return dataset.repeat(num_client).shuffle(shuffle_buffer, seed=global_seed).map(
+            custom_augment, num_parallel_calls=tf.data.AUTOTUNE).batch(
+            batch_zize).prefetch(prefetch_buffer)
