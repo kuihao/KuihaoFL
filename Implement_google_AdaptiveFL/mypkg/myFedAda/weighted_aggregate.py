e3fdb865d3e7f348e144622c98e839af9c0561f1
kuihao <kuihaochang@gmail.com>
2021-11-19 22:32:45 +0800

    change learning rate position

--- Implement_google_AdaptiveFL/mypkg/myFedAda/weighted_aggregate.py
+++ Implement_google_AdaptiveFL/mypkg/myFedAda/weighted_aggregate.py
@@ -2,7 +2,7 @@ from flwr.common import Weights
 from functools import reduce
 from typing import List, Tuple
 import numpy as np
-def Weighted_Aggregate(results: List[Tuple[Weights, int]], learning_rate = 1) -> Weights:
+def Weighted_Aggregate(results: List[Tuple[Weights, int]], learning_rate=1) -> Weights:
   """Compute weighted average."""
   # Calculate the total number of examples used during training
   num_examples_total = sum([num_examples for _, num_examples in results])
@@ -14,7 +14,7 @@ def Weighted_Aggregate(results: List[Tuple[Weights, int]], learning_rate = 1) ->
 
   # Compute average weights of each layer
   weights_prime: Weights = [
-      learning_rate * (reduce(np.add, layer_updates) / num_examples_total)
+       (reduce(np.add, layer_updates) / num_examples_total)
       for layer_updates in zip(*weighted_weights)
   ]
-  return weights_prime
+  return learning_rate*weights_prime
