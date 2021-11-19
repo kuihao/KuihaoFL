2929078d988e9a36da3ea07488839a551bdc5d2e
kuihao <kuihaochang@gmail.com>
2021-11-19 20:18:53 +0800

    clear unuse link

--- Implement_google_AdaptiveFL/mypkg/TF/mymodel/__init__.py
+++
@@ -1,8 +0,0 @@
-from .cnn import CNN_Model
-from .resnet18 import myResNet
-from .preprocess import GoogleAdaptive_tfds_preprocess
-__all__ = [
-    "CNN_Model",
-    "myResNet",
-    "GoogleAdaptive_tfds_preprocess",
-]
