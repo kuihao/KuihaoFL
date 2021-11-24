9d5da04781117f529ed8b95ec6db6c9017e530ec
Kuihao <41053A041@gms.ndhu.edu.tw>
2021-11-24 18:22:54 +0800

    experiment testing the logger and yogi

--- Implement_google_AdaptiveFL/shell/fl_simulation.sh
+++ Implement_google_AdaptiveFL/shell/fl_simulation.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 # This file is called ~/script.sh
-screen -S FL-server -m bash -c 'python kuihao_fl_sequential_multi_clients_simulation.py -m 0 -n cifar100_noniid_resnet18_fedavgm_10client_4000round_mountum09_leta1em3d2_seta1e0;echo "**end of file**";$SHELL'
+#screen -S FL-server -m bash -c 'python kuihao_fl_sequential_multi_clients_simulation.py -m 0 -n cifar100_noniid_resnet18_fedavgm_10client_4000round_mountum09_leta1em3d2_seta1e0;echo "**end of file**";$SHELL'
+# python kuihao_fl_sequential_multi_clients_simulation.py -m 0 -n cifar100_noniid_resnet18_fedyogi
+screen -S FL-server -m bash -c 'python kuihao_fl_sequential_multi_clients_simulation.py -m 0 -n cifar100_noniid_resnet18_fedyogi;echo "**end of file**";$SHELL'
--- Implement_google_AdaptiveFL/test.py
+++ Implement_google_AdaptiveFL/test.py
@@ -1,1 +1,12 @@
-from mypkg.TF import CNN_Model, myResNet, GoogleAdaptive_tfds_preprocess
+'''
+from mypkg import mylog, secure_mkdir
+
+model_name = 'test'
+log_folder = secure_mkdir("FL_log"+"/"+model_name)
+
+log_text = f'*** FL Traing Record ***\n' \
+           f'77777'
+
+mylog(log_text,log_folder+'/log')'''
+vari, vari_name = 123, '2222222'
+print(vari, vari_name)
