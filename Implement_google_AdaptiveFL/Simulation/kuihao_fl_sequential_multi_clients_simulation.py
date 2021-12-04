618f305cbfcf131329ee080838a217494e51da41
Kuihao <41053A041@gms.ndhu.edu.tw>
2021-12-04 16:44:26 +0800

    add the secure saving with interrupt happened

--- Implement_google_AdaptiveFL/Simulation/kuihao_fl_sequential_multi_clients_simulation.py
+++ Implement_google_AdaptiveFL/Simulation/kuihao_fl_sequential_multi_clients_simulation.py
@@ -18,10 +18,10 @@ import os
 # Make TensorFlow logs less verbose
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-#import sys
+import sys
 # add path to use my package
 #sys.path.append('/Implement_FedAdativate')#/home/sheng/document/Kuihao
-
+from datetime import datetime
 import numpy as np
 
 from mypkg import (
@@ -102,12 +102,12 @@ HypHyperSet_Train_EveryRound_client_number = 10
 HyperSet_Test_all_connect_client_number = 100
 HypHyperSet_Test_EveryRound_client_number = 10
 
-HyperSet_Server_eta = pow(10,(-1)) #1e-3
-HyperSet_Server_tau = pow(10,(-3)) #1e-2
+HyperSet_Server_eta = pow(10,(-3)) #1e-3
+HyperSet_Server_tau = pow(10,(-1)) #1e-2
 HyperSet_Server_beta1 = 0.9 
 HyperSet_Server_beta2 = 0.99
 
-HyperSet_Local_eta = pow(10,(-2)) #1e-1
+HyperSet_Local_eta = pow(10,(-1)) #1e-1
 HyperSet_Local_momentum = 0.9
 HyperSet_Local_batch_size = 20
 HyperSet_Local_epoch = 1
@@ -184,154 +184,209 @@ Testing_result_centralized = {'loss':[],'accuracy':[],'sparse_top_k_categorical_
 """
 #FL Learning
 """
-for rnd in range(HyperSet_round):
-  print(f"**** Round {rnd+1} ****")
-  
-  # Client Train
-  for client_i in range(HypHyperSet_Train_EveryRound_client_number):
-    # get client id
-    cid = SampleClientID_np[client_i,rnd]
-    #print(f"\nClient {client_i} execute cid={cid}")
+try:
+  rnd=0
+  for rnd in range(HyperSet_round):
+    print(f"**** Round {rnd+1} ****")
     
-    # load data
-    tfds_train = myLoadDS(dataset_path+f'/client/train/client_{cid}_train', 'tfds')
-    #tfds_train = tf.data.experimental.load(dataset_path+f'/client/train/client_{cid}_train')
-    train_len = 100 #sum(1 for _ in tfds_train)
+    # Client Train
+    for client_i in range(HypHyperSet_Train_EveryRound_client_number):
+      # get client id
+      cid = SampleClientID_np[client_i,rnd]
+      #print(f"\nClient {client_i} execute cid={cid}")
+      
+      # load data
+      tfds_train = myLoadDS(dataset_path+f'/client/train/client_{cid}_train', 'tfds')
+      #tfds_train = tf.data.experimental.load(dataset_path+f'/client/train/client_{cid}_train')
+      train_len = 100 #sum(1 for _ in tfds_train)
+      
+      # Local model train
+      model.set_weights(GlobalModel_NewestWeight)
+      history = model.fit(
+                  preprocessor.preprocess(tfds_train, rng, train=True, BruteForce_kill_nan=True, add_minmax=False, normal_mode=False),
+                  #tfds_train.map(simple_cifar100_preprocessor, num_parallel_calls=tf.data.AUTOTUNE).batch(HyperSet_Local_batch_size).prefetch(20),
+                  epochs=HyperSet_Local_epoch,
+                  verbose=3,
+                )
+      results = {
+          "loss": history.history["loss"][-1],
+          "accuracy": history.history["accuracy"][-1],
+          "sparse_top_k_categorical_accuracy": history.history["sparse_top_k_categorical_accuracy"][-1],
+      }
+      Clients_ModelWeights_list[client_i] = model.get_weights()
+      Clients_DataSize_np[client_i] = train_len
+      Clients_Results_list[client_i] = results
+
+    # Aggregation
+    ReturnResults = [(weight,size) for weight,size in zip(Clients_ModelWeights_list,Clients_DataSize_np)]
+    GlobalModel_NewestWeight = HyperSet_Aggregation(GlobalModel_NewestWeight,ReturnResults,HyperSet_Server_eta,HyperSet_Server_tau) #fl.common.parameters_to_weights()
+
+    # Aggregate clients' training results (loss, acc., top-k-acc.)
+    all_dataset_size = Clients_DataSize_np.sum()
+
+    losses_weighted = [r['loss'] * size for r,size in zip(Clients_Results_list,Clients_DataSize_np)]
+    loss_aggregated = sum(losses_weighted) / all_dataset_size
+
+    accuracies_weighted = [r['accuracy'] * size for r,size in zip(Clients_Results_list,Clients_DataSize_np)]
+    accuracy_aggregated = sum(accuracies_weighted) / all_dataset_size
+
+    topK_accuracies_weighted = [r['sparse_top_k_categorical_accuracy'] * size for r,size in zip(Clients_Results_list,Clients_DataSize_np)]
+    topK_accuracies_aggregated = sum(topK_accuracies_weighted) / all_dataset_size
     
-    # Local model train
+    # 輸出儲存 Client-side 訓練結果
+    print(f"****\nRound {rnd+1}, train results from clients after aggregation:\n"\
+    f"Loss:{loss_aggregated} Acc.:{accuracy_aggregated} TopK Acc.:{topK_accuracies_aggregated}"\
+    f"\n****")
+    Training_result_distributed["loss"].append(loss_aggregated)
+    Training_result_distributed["accuracy"].append(accuracy_aggregated)
+    Training_result_distributed["sparse_top_k_categorical_accuracy"].append(topK_accuracies_aggregated)
+
+    '''
+    # 輸出儲存 Server-side 測試結果
+    print("**** Server-side evaluate:")
     model.set_weights(GlobalModel_NewestWeight)
-    history = model.fit(
-                preprocessor.preprocess(tfds_train, rng, train=True, BruteForce_kill_nan=True, add_minmax=False, normal_mode=False),
-                #tfds_train.map(simple_cifar100_preprocessor, num_parallel_calls=tf.data.AUTOTUNE).batch(HyperSet_Local_batch_size).prefetch(20),
-                epochs=HyperSet_Local_epoch,
-                verbose=3,
-              )
-    results = {
-        "loss": history.history["loss"][-1],
-        "accuracy": history.history["accuracy"][-1],
-        "sparse_top_k_categorical_accuracy": history.history["sparse_top_k_categorical_accuracy"][-1],
-    }
-    Clients_ModelWeights_list[client_i] = model.get_weights()
-    Clients_DataSize_np[client_i] = train_len
-    Clients_Results_list[client_i] = results
+    loss_aggregated, accuracy_aggregated, topK_accuracies_aggregated = model.evaluate( 
+            preprocessor.preprocess(server_test_data, rng, train=False, add_minmax=True),
+            #server_test_data.map(simple_cifar100_preprocessor, num_parallel_calls=tf.data.AUTOTUNE).batch(HyperSet_Local_batch_size).prefetch(20),
+            verbose=2,
+          )
+    print("***")
+    Testing_result_centralized["loss"].append(loss_aggregated)
+    Testing_result_centralized["accuracy"].append(accuracy_aggregated)
+    Testing_result_centralized["sparse_top_k_categorical_accuracy"].append(topK_accuracies_aggregated)
+    '''
 
-  # Aggregation
-  ReturnResults = [(weight,size) for weight,size in zip(Clients_ModelWeights_list,Clients_DataSize_np)]
-  GlobalModel_NewestWeight = HyperSet_Aggregation(GlobalModel_NewestWeight,ReturnResults,HyperSet_Server_eta,HyperSet_Server_tau) #fl.common.parameters_to_weights()
-
-  # Aggregate clients' training results (loss, acc., top-k-acc.)
-  all_dataset_size = Clients_DataSize_np.sum()
-
-  losses_weighted = [r['loss'] * size for r,size in zip(Clients_Results_list,Clients_DataSize_np)]
-  loss_aggregated = sum(losses_weighted) / all_dataset_size
-
-  accuracies_weighted = [r['accuracy'] * size for r,size in zip(Clients_Results_list,Clients_DataSize_np)]
-  accuracy_aggregated = sum(accuracies_weighted) / all_dataset_size
-
-  topK_accuracies_weighted = [r['sparse_top_k_categorical_accuracy'] * size for r,size in zip(Clients_Results_list,Clients_DataSize_np)]
-  topK_accuracies_aggregated = sum(topK_accuracies_weighted) / all_dataset_size
-  
-  # 輸出儲存 Client-side 訓練結果
-  print(f"****\nRound {rnd+1}, train results from clients after aggregation:\n"\
-  f"Loss:{loss_aggregated} Acc.:{accuracy_aggregated} TopK Acc.:{topK_accuracies_aggregated}"\
-  f"\n****")
-  Training_result_distributed["loss"].append(loss_aggregated)
-  Training_result_distributed["accuracy"].append(accuracy_aggregated)
-  Training_result_distributed["sparse_top_k_categorical_accuracy"].append(topK_accuracies_aggregated)
-
-  '''
-  # 輸出儲存 Server-side 測試結果
-  print("**** Server-side evaluate:")
-  model.set_weights(GlobalModel_NewestWeight)
-  loss_aggregated, accuracy_aggregated, topK_accuracies_aggregated = model.evaluate( 
-          preprocessor.preprocess(server_test_data, rng, train=False, add_minmax=True),
-          #server_test_data.map(simple_cifar100_preprocessor, num_parallel_calls=tf.data.AUTOTUNE).batch(HyperSet_Local_batch_size).prefetch(20),
-          verbose=2,
-         )
-  print("***")
-  Testing_result_centralized["loss"].append(loss_aggregated)
-  Testing_result_centralized["accuracy"].append(accuracy_aggregated)
-  Testing_result_centralized["sparse_top_k_categorical_accuracy"].append(topK_accuracies_aggregated)
-  '''
-
-  
-  # Client Test
-  for client_i in range(HypHyperSet_Test_EveryRound_client_number):
-    # get client id
-    cid = SampleClientID_test_np[client_i,rnd]
-    #print(f"\nClient {client_i} test cid={cid}")
     
-    # load data
-    tfds_test = myLoadDS(dataset_path+f'/client/test/client_{cid}_train', 'tfds')
-    #tfds_test = tf.data.experimental.load(dataset_path+f'/client/test/client_{cid%100}_train')
-    val_len = 100 #sum(1 for _ in tfds_test)
+    # Client Test
+    for client_i in range(HypHyperSet_Test_EveryRound_client_number):
+      # get client id
+      cid = SampleClientID_test_np[client_i,rnd]
+      #print(f"\nClient {client_i} test cid={cid}")
+      
+      # load data
+      tfds_test = myLoadDS(dataset_path+f'/client/test/client_{cid}_train', 'tfds')
+      #tfds_test = tf.data.experimental.load(dataset_path+f'/client/test/client_{cid%100}_train')
+      val_len = 100 #sum(1 for _ in tfds_test)
+      
+      # Local model train
+      model.set_weights(GlobalModel_NewestWeight)
+      loss, acc, topk = model.evaluate( 
+                        preprocessor.preprocess(tfds_test, rng, train=False, BruteForce_kill_nan=True, add_minmax=False, normal_mode=False),
+                        #tfds_test.map(simple_cifar100_preprocessor, num_parallel_calls=tf.data.AUTOTUNE).batch(HyperSet_Local_batch_size).prefetch(20),
+                        verbose=3,
+                        )
+      results = {
+          "loss": loss,
+          "accuracy": acc,
+          "sparse_top_k_categorical_accuracy": topk,
+      }
+      Clients_DataSize_test_np[client_i] = val_len
+      Clients_Results_test_list[client_i] = results
     
-    # Local model train
-    model.set_weights(GlobalModel_NewestWeight)
-    loss, acc, topk = model.evaluate( 
-                      preprocessor.preprocess(tfds_test, rng, train=False, BruteForce_kill_nan=True, add_minmax=False, normal_mode=False),
-                      #tfds_test.map(simple_cifar100_preprocessor, num_parallel_calls=tf.data.AUTOTUNE).batch(HyperSet_Local_batch_size).prefetch(20),
-                      verbose=3,
-                      )
-    results = {
-        "loss": loss,
-        "accuracy": acc,
-        "sparse_top_k_categorical_accuracy": topk,
-    }
-    Clients_DataSize_test_np[client_i] = val_len
-    Clients_Results_test_list[client_i] = results
-  
-  # Aggregate clients' testing results (loss, acc., top-k-acc.)
-  all_dataset_size = Clients_DataSize_test_np.sum()
+    # Aggregate clients' testing results (loss, acc., top-k-acc.)
+    all_dataset_size = Clients_DataSize_test_np.sum()
 
-  losses_weighted = [r['loss'] * size for r,size in zip(Clients_Results_test_list,Clients_DataSize_test_np)]
-  loss_aggregated = sum(losses_weighted) / all_dataset_size
+    losses_weighted = [r['loss'] * size for r,size in zip(Clients_Results_test_list,Clients_DataSize_test_np)]
+    loss_aggregated = sum(losses_weighted) / all_dataset_size
 
-  accuracies_weighted = [r['accuracy'] * size for r,size in zip(Clients_Results_test_list,Clients_DataSize_test_np)]
-  accuracy_aggregated = sum(accuracies_weighted) / all_dataset_size
+    accuracies_weighted = [r['accuracy'] * size for r,size in zip(Clients_Results_test_list,Clients_DataSize_test_np)]
+    accuracy_aggregated = sum(accuracies_weighted) / all_dataset_size
 
-  topK_accuracies_weighted = [r['sparse_top_k_categorical_accuracy'] * size for r,size in zip(Clients_Results_test_list,Clients_DataSize_test_np)]
-  topK_accuracies_aggregated = sum(topK_accuracies_weighted) / all_dataset_size
-  
-  # 輸出儲存 Client-side 測試結果
-  print(f"****\nRound {rnd+1}, test results from clients after aggregation:\n"\
-  f"Loss:{loss_aggregated} Acc.:{accuracy_aggregated} TopK Acc.:{topK_accuracies_aggregated}"\
-  f"\n****")
-  Testing_result_distributed["loss"].append(loss_aggregated)
-  Testing_result_distributed["accuracy"].append(accuracy_aggregated)
-  Testing_result_distributed["sparse_top_k_categorical_accuracy"].append(topK_accuracies_aggregated)
-  
-  # 定期備份
-  if SAVE:
-    FL_Results_folder = secure_mkdir("FL_Results"+"/"+model_name)
-    if rnd%1000==0:
-      if Training_result_distributed is not None:
-          np.savez(f"{FL_Results_folder}/Training_result_distributed.npz", Training_result_distributed)
-      if Testing_result_distributed is not None:
-          np.savez(f"{FL_Results_folder}/Testing_result_distributed.npz", Testing_result_distributed)
-      if Testing_result_centralized is not None:
-          np.savez(f"{FL_Results_folder}/Testing_result_centralized.npz", Testing_result_centralized)
-    elif rnd+1 == HyperSet_round:
-      if Training_result_distributed is not None:
-          np.savez(f"{FL_Results_folder}/Training_result_distributed.npz", Training_result_distributed)
-      if Testing_result_distributed is not None:
-          np.savez(f"{FL_Results_folder}/Testing_result_distributed.npz", Testing_result_distributed)
-      if Testing_result_centralized is not None:
-          np.savez(f"{FL_Results_folder}/Testing_result_centralized.npz", Testing_result_centralized)
+    topK_accuracies_weighted = [r['sparse_top_k_categorical_accuracy'] * size for r,size in zip(Clients_Results_test_list,Clients_DataSize_test_np)]
+    topK_accuracies_aggregated = sum(topK_accuracies_weighted) / all_dataset_size
+    
+    # 輸出儲存 Client-side 測試結果
+    print(f"****\nRound {rnd+1}, test results from clients after aggregation:\n"\
+    f"Loss:{loss_aggregated} Acc.:{accuracy_aggregated} TopK Acc.:{topK_accuracies_aggregated}"\
+    f"\n****")
+    Testing_result_distributed["loss"].append(loss_aggregated)
+    Testing_result_distributed["accuracy"].append(accuracy_aggregated)
+    Testing_result_distributed["sparse_top_k_categorical_accuracy"].append(topK_accuracies_aggregated)
+    
+    # 定期備份
+    if SAVE:
+      FL_Results_folder = secure_mkdir("FL_Results"+"/"+model_name)
+      if rnd%1000==0:
+        if Training_result_distributed is not None:
+            np.savez(f"{FL_Results_folder}/Training_result_distributed.npz", Training_result_distributed)
+        if Testing_result_distributed is not None:
+            np.savez(f"{FL_Results_folder}/Testing_result_distributed.npz", Testing_result_distributed)
+        if Testing_result_centralized is not None:
+            np.savez(f"{FL_Results_folder}/Testing_result_centralized.npz", Testing_result_centralized)
+      elif rnd+1 == HyperSet_round:
+        if Training_result_distributed is not None:
+            np.savez(f"{FL_Results_folder}/Training_result_distributed.npz", Training_result_distributed)
+        if Testing_result_distributed is not None:
+            np.savez(f"{FL_Results_folder}/Testing_result_distributed.npz", Testing_result_distributed)
+        if Testing_result_centralized is not None:
+            np.savez(f"{FL_Results_folder}/Testing_result_centralized.npz", Testing_result_centralized)
 
-  if SAVE:
-    checkpoint_folder = secure_mkdir("ckpoint"+"/"+model_name)
-    if rnd%1000==0:
-      print(f"****Saving round {rnd+1} aggregated_weights...****")
-      np.savez(f"{checkpoint_folder}/round-{rnd+1}-weights.npz", *GlobalModel_NewestWeight)
-    elif rnd+1 == HyperSet_round:
-      print(f"****Saving round {rnd+1} aggregated_weights...****")
-      np.savez(f"{checkpoint_folder}/round-{rnd+1}-weights.npz", *GlobalModel_NewestWeight)
+    if SAVE:
+      checkpoint_folder = secure_mkdir("ckpoint"+"/"+model_name)
+      if rnd%1000==0:
+        print(f"****Saving round {rnd+1} aggregated_weights...****")
+        np.savez(f"{checkpoint_folder}/round-{rnd+1}-weights.npz", *GlobalModel_NewestWeight)
+      elif rnd+1 == HyperSet_round:
+        print(f"****Saving round {rnd+1} aggregated_weights...****")
+        np.savez(f"{checkpoint_folder}/round-{rnd+1}-weights.npz", *GlobalModel_NewestWeight)
 
-  print('\n')
+    print('\n')
+
+# 緊急狀況備份
+except KeyboardInterrupt or InterruptedError:
+  print("KeyboardInterrupt or InterruptedError!!")
+  print("Saving model...")
+  np.savez(f"{checkpoint_folder}/round-{rnd+1}-weights.npz", *GlobalModel_NewestWeight)
+  print("Model saved.")
+
+  print("Saving result...")
+  if Training_result_distributed is not None:
+    np.savez(f"{FL_Results_folder}/Training_result_distributed.npz", Training_result_distributed)
+  if Testing_result_distributed is not None:
+    np.savez(f"{FL_Results_folder}/Testing_result_distributed.npz", Testing_result_distributed)
+  if Testing_result_centralized is not None:
+    np.savez(f"{FL_Results_folder}/Testing_result_centralized.npz", Testing_result_centralized)
+  print("Result saved.")
+
+  print("Logging...")
+  now_time = datetime.now()
+  time_str = now_time.strftime("%m_%d_%Y__%H_%M_%S")
+  log_folder = secure_mkdir("FL_log"+"/"+"InterruptSaved_"+model_name)
+  log_text = f'*** FL Traing Record ***\n \
+             *[This training was unexpectly interrupted.]*\n \
+             *[Stop at round = {rnd}]*\n \
+             Model Name: {model_name}\n \
+             FL Finish Time: {time_str}\n \
+             \n--- FL setting ---\n \
+             Aggregation: {Aggregation_name}\n \
+             Rounds: {HyperSet_round}\n \
+             Traing population: {HyperSet_Train_all_connect_client_number}\n \
+             Testing population: {HyperSet_Test_all_connect_client_number}\n \
+             Number of client per round: {HypHyperSet_Train_EveryRound_client_number}\n \
+             \n--- Server-side hyperparemeter ---\n \
+             Learning-rate: {HyperSet_Server_eta}\n \
+             Tau: {HyperSet_Server_tau}\n \
+             Beta-1: {HyperSet_Server_beta1}\n \
+             Beta-2: {HyperSet_Server_beta2}\n \
+             \n--- Client-side hyperparemeter ---\n \
+             Learning-rate: {HyperSet_Local_eta}\n \
+             Momentum: {HyperSet_Local_momentum}\n \
+             Local epoch: {HyperSet_Local_epoch}\n \
+             Local batch size: {HyperSet_Local_batch_size}\n \
+             \n--- Other env. setting ---\n \
+             Random Seed: {SEED}\n \
+             \n--- Result ---\n \
+             *Last Train Acc.: {Training_result_distributed["accuracy"][-1]}\n \
+             Last Train TopK-Acc.: {Training_result_distributed["sparse_top_k_categorical_accuracy"][-1]}\n \
+             Last Train Loss: {Training_result_distributed["loss"][-1]}\n \
+             *Last Test Acc.: {Testing_result_distributed["accuracy"][-1]}\n \
+             Last Test TopK-Acc.: {Testing_result_distributed["sparse_top_k_categorical_accuracy"][-1]}\n \
+             Last Test Loss: {Testing_result_distributed["loss"][-1]}\n'
+  mylog(log_text, log_folder+'/log')
+  print("Log saved.")
+  sys.exit()
 
 if SAVE:
-  from datetime import datetime
   now_time = datetime.now()
   time_str = now_time.strftime("%m_%d_%Y__%H_%M_%S")
   N = 100 # To calculate the avg N rounds result.
@@ -347,7 +402,7 @@ if SAVE:
              f'Rounds: {HyperSet_round}\n' \
              f'Traing population: {HyperSet_Train_all_connect_client_number}\n' \
              f'Testing population: {HyperSet_Test_all_connect_client_number}\n' \
-             f'Number of client per round:{HypHyperSet_Train_EveryRound_client_number}\n' \
+             f'Number of client per round: {HypHyperSet_Train_EveryRound_client_number}\n' \
              f'\n--- Server-side hyperparemeter ---\n' \
              f'Learning-rate: {HyperSet_Server_eta}\n' \
              f'Tau: {HyperSet_Server_tau}\n' \
--- Implement_google_AdaptiveFL/test.py
+++ Implement_google_AdaptiveFL/test.py
@@ -1,6 +1,11 @@
-import sys
-test = ''
-sys.stdout = open('TMP.txt', 'w')
-for i in range(10):
-    test = str(i)
-    print(test)
+A = 12.3
+B = A
+C = f'{B}'
+
+print(B)
+print(C)
+
+    
+
+
+
